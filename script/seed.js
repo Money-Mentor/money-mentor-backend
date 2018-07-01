ac5379e2a32a653048a4c79e2cb9d508037f9c24
PanktiParikh <panktip85@gmail.com>
2018-07-01 10:50:40 -0400

    assed seed file for Seri & Joyce

--- script/seed.js
+++ script/seed.js
@@ -2,25 +2,95 @@
 
 const db = require('../server/db');
 const { User, Transaction, Account, Item } = require('../server/db/models');
-// const allTransactions = require('./transactionSeed')
 
-/**
- * Welcome to the seed file! This seed file uses a newer language feature called...
- *
- *                  -=-= ASYNC...AWAIT -=-=
- *
- * Async-await is a joy to use! Read more about it in the MDN docs:
- *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
- *
- * Now that you've got the main idea, check it out in practice below!
- */
+const taxiDateArr = doTimes(10);
+const dateArr = doTimes(30);
+const payday = [
+  '2018-05-11',
+  '2018-05-25',
+  '2018-06-08',
+  '2018-07-22',
+  '2018-07-06',
+];
+const onceAMonth = ['2018-05-01', '2018-06-01', '2018-07-01'];
+
+const coffeeShop = [
+  'Starbucks',
+  'Gregorys Coffee',
+  'Bluestone Lane',
+  'Joe and the Juice',
+];
+const restaurant = ['Sweet Green', 'Open Market', 'Dig In', 'Go GO Curry'];
+const grocery = [
+  'Whole Foods',
+  `Trader Joe's`,
+  'Westside Market',
+  '55 Fulton Market',
+];
+const shops = ['Zara', `Reformation`, 'Express', 'Club Monaco'];
+
+const taxi = ['Yellow Cab', 'Uber', 'Lyft', 'Juno'];
+
+function randomStore(store) {
+  return store[Math.floor(Math.random() * 4)];
+}
+
+function randomDate(start, end) {
+  var d = new Date(
+      start.getTime() + Math.random() * (end.getTime() - start.getTime())
+    ),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+
+  return [year, month, day].join('-');
+}
+
+function doTimes(n) {
+  const results = [];
+  while (n--) {
+    results.push(randomDate(new Date(2018, 5, 1), new Date()));
+  }
+  return results;
+}
+function randomBigAmount() {
+  return Math.floor(Math.random() * 40) + 30;
+}
+
+function randomAmount() {
+  return Math.floor(Math.random() * 30) + 10;
+}
+function randomAmountSmall() {
+  return Math.floor(Math.random() * 5) + 5;
+}
 
 async function seed() {
   await db.sync({ force: true });
   console.log('db synced!');
   // Whoa! Because we `await` the promise that db.sync returns, the next line will not be
   // executed until that promise resolves!
+
+  const users = await Promise.all([
+    User.create({
+      email: 'cody@email.com',
+      password: '123',
+    }),
+    User.create({ email: '1', password: '1' }),
+  ]);
+
+  const trans = await Promise.all([
+    Transaction.create({
+      name: 'Starbucks',
+      amount: 10,
+      date: '2018-6-29',
+      category1: 'Food and Drink',
+      category2: 'Coffee Shop',
+      userId: 3,
+    }),
+  ]);
 
   /*-------------------- USERS ----------------------*/
   const joyce = await User.create({
@@ -45,7 +115,7 @@ async function seed() {
   });
 
   /*-------------------- ACCOUNTS ----------------------*/
-  const joyceChaseChecking = await Account.create({
+  const joyceChecking = await Account.create({
     account_id: 'joyceChaseAccount',
     current_balance: 5000,
     available_balance: 5000,
@@ -53,7 +123,7 @@ async function seed() {
     userId: joyce.id,
     bankId: joyceItem.id,
   });
-  const joyceChaseCredit = await Account.create({
+  const joyceCredit = await Account.create({
     account_id: 'joyceChaseCredit',
     current_balance: 2000,
     available_balance: 2000,
@@ -61,7 +131,7 @@ async function seed() {
     userId: joyce.id,
     bankId: joyceItem.id,
   });
-  const joyceChaseSaving = await Account.create({
+  const joyceSaving = await Account.create({
     account_id: 'joyceChaseSaving',
     current_balance: 10000,
     available_balance: 10000,
@@ -69,7 +139,7 @@ async function seed() {
     userId: joyce.id,
   });
 
-  const sheriChaseChecking = await Account.create({
+  const sheriChecking = await Account.create({
     account_id: 'sheriChaseChecking',
     current_balance: 4000,
     available_balance: 4000,
@@ -77,7 +147,7 @@ async function seed() {
     userId: sheri.id,
     bankId: sheriItem.id,
   });
-  const sheriChaseCredit = await Account.create({
+  const sheriCredit = await Account.create({
     account_id: 'sheriChaseCredit',
     current_balance: 3000,
     available_balance: 3000,
@@ -85,7 +155,7 @@ async function seed() {
     userId: sheri.id,
     bankId: sheriItem.id,
   });
-  const sheriChaseSaving = await Account.create({
+  const sheriSaving = await Account.create({
     account_id: 'sheriChaseSaving',
     current_balance: 12000,
     available_balance: 12000,
@@ -95,28 +165,357 @@ async function seed() {
   });
 
   /*-------------------- TRANSACTIONS ---------------------*/
-  // const transactions = await Promise.all(
-  //   allTransactions.map(transaction => Transaction.create(transaction))
-  // );
-
-  const users = await Promise.all([
-    User.create({
-      email: 'cody@email.com',
-      password: '123',
+  const allTransactions = [
+    ...payday.map(day => {
+      return {
+        name: 'Google Payroll',
+        amount: -2200,
+        date: day,
+        accountId: joyceChecking.account_id,
+        category: 'Transfer',
+        category2: 'Payroll',
+        userId: joyce.id,
+      };
     }),
-    User.create({ email: '1', password: '1' }),
-  ]);
-
-  const trans = await Promise.all([
-    Transaction.create({
-      name: 'Starbucks',
-      amount: 10,
-      date: '2018-6-29',
-      category1: 'Food and Drink',
-      category2: 'Coffee Shop',
-      userId: 3,
+    ...payday.map(day => {
+      return {
+        name: 'Google Payroll',
+        amount: -500,
+        date: day,
+        accountId: joyceSaving.account_id,
+        category: 'Transfer',
+        category2: 'Payroll',
+        userId: joyce.id,
+      };
     }),
-  ]);
+    ...onceAMonth.map(day => {
+      return {
+        name: 'Credit Card Payment',
+        amount: 200,
+        date: day,
+        accountId: joyceChecking.account_id,
+        category: 'Payment',
+        category2: 'Credit Card',
+        userId: joyce.id,
+      };
+    }),
+    ...onceAMonth.map(day => {
+      return {
+        name: 'Rent',
+        amount: 1500,
+        date: day,
+        accountId: joyceChecking.account_id,
+        category: 'Payment',
+        category2: 'Rent',
+        userId: joyce.id,
+      };
+    }),
+    ...onceAMonth.map(day => {
+      return {
+        name: 'ConEd',
+        amount: 50,
+        date: day,
+        accountId: joyceChecking.account_id,
+        category: 'Payment',
+        category2: 'Electric Bill',
+        userId: joyce.id,
+      };
+    }),
+    ...dateArr.map(day => {
+      return {
+        name: randomStore(coffeeShop),
+        amount: randomAmountSmall(),
+        date: day,
+        accountId: joyceCredit.account_id,
+        category: 'Food and Drink',
+        category2: 'Coffee Shop',
+        userId: joyce.id,
+      };
+    }),
+    ...dateArr.map(day => {
+      return {
+        name: randomStore(restaurant),
+        amount: randomAmountSmall(),
+        date: day,
+        accountId: joyceCredit.account_id,
+        category: 'Food and Drink',
+        category2: 'Restaurants',
+        userId: joyce.id,
+      };
+    }),
+    ...dateArr.map(day => {
+      return {
+        name: randomStore(grocery),
+        amount: randomAmount(),
+        date: day,
+        accountId: joyceCredit.account_id,
+        category: 'Shops',
+        category2: 'Supermarkets and Groceries',
+        userId: joyce.id,
+      };
+    }),
+    ...dateArr.map(day => {
+      return {
+        name: `Killarney Rose`,
+        amount: randomAmount(),
+        date: day,
+        accountId: joyceCredit.account_id,
+        category: 'Shops',
+        category2: 'Supermarkets and Groceries',
+        userId: joyce.id,
+      };
+    }),
+    ...dateArr.map(day => {
+      return {
+        name: `Amazon`,
+        amount: randomAmount(),
+        date: day,
+        accountId: joyceCredit.account_id,
+        category: 'Food and Drink',
+        category2: 'Bar',
+        userId: joyce.id,
+      };
+    }),
+    ...taxiDateArr.map(day => {
+      return {
+        name: randomStore(taxi),
+        amount: randomAmount(),
+        date: day,
+        accountId: joyceCredit.account_id,
+        category: 'Travel',
+        category2: 'Car Service',
+        userId: joyce.id,
+      };
+    }),
+    ...taxiDateArr.map(day => {
+      return {
+        name: randomStore(shops),
+        amount: randomBigAmount(),
+        date: day,
+        accountId: joyceCredit.account_id,
+        category: 'Shops',
+        category2: 'Clothing',
+        userId: joyce.id,
+      };
+    }),
+    {
+      name: 'Madison Bicycle Shop',
+      amount: 50,
+      date: '2018-06-12',
+      accountId: joyceCredit.account_id,
+      category: 'Shops',
+      category2: 'Bicycles',
+      userId: joyce.id,
+    },
+    {
+      name: 'United Airlines',
+      amount: 350,
+      date: '2018-06-19',
+      accountId: joyceCredit.account_id,
+      category: 'Travel',
+      category2: 'Airlines and Aviation Services',
+      userId: joyce.id,
+    },
+    ...onceAMonth.map(day => {
+      return {
+        name: 'Netflix',
+        amount: 12,
+        date: day,
+        accountId: joyceCredit.account_id,
+        category: 'Service',
+        category2: 'Entertainment',
+        userId: joyce.id,
+      };
+    }),
+    ...onceAMonth.map(day => {
+      return {
+        name: 'Touchstone CLimbing',
+        amount: 79,
+        date: day,
+        accountId: joyceCredit.account_id,
+        category: 'Recreation',
+        category2: 'Gyms and Fitness Centers',
+        userId: joyce.id,
+      };
+    }),
+    /*----------- SHERI SEED FILE-----------------*/
+    ...payday.map(day => {
+      return {
+        name: 'Facebook Payroll',
+        amount: -2200,
+        date: day,
+        accountId: sheriChecking.account_id,
+        category: 'Transfer',
+        category2: 'Payroll',
+        userId: sheri.id,
+      };
+    }),
+    ...payday.map(day => {
+      return {
+        name: 'FacebookPayroll',
+        amount: -500,
+        date: day,
+        accountId: sheriSaving.account_id,
+        category: 'Transfer',
+        category2: 'Payroll',
+        userId: sheri.id,
+      };
+    }),
+    ...onceAMonth.map(day => {
+      return {
+        name: 'Credit Card Payment',
+        amount: 200,
+        date: day,
+        accountId: sheriChecking.account_id,
+        category: 'Payment',
+        category2: 'Credit Card',
+        userId: sheri.id,
+      };
+    }),
+    ...onceAMonth.map(day => {
+      return {
+        name: 'Rent',
+        amount: 1500,
+        date: day,
+        accountId: sheriChecking.account_id,
+        category: 'Payment',
+        category2: 'Rent',
+        userId: sheri.id,
+      };
+    }),
+    ...onceAMonth.map(day => {
+      return {
+        name: 'ConEd',
+        amount: 50,
+        date: day,
+        accountId: sheriChecking.account_id,
+        category: 'Payment',
+        category2: 'Electric Bill',
+        userId: sheri.id,
+      };
+    }),
+    ...dateArr.map(day => {
+      return {
+        name: randomStore(coffeeShop),
+        amount: randomAmountSmall(),
+        date: day,
+        accountId: sheriCredit.account_id,
+        category: 'Food and Drink',
+        category2: 'Coffee Shop',
+        userId: sheri.id,
+      };
+    }),
+    ...dateArr.map(day => {
+      return {
+        name: randomStore(restaurant),
+        amount: randomAmountSmall(),
+        date: day,
+        accountId: sheriCredit.account_id,
+        category: 'Food and Drink',
+        category2: 'Restaurants',
+        userId: sheri.id,
+      };
+    }),
+    ...dateArr.map(day => {
+      return {
+        name: randomStore(grocery),
+        amount: randomAmount(),
+        date: day,
+        accountId: sheriCredit.account_id,
+        category: 'Shops',
+        category2: 'Supermarkets and Groceries',
+        userId: sheri.id,
+      };
+    }),
+    ...dateArr.map(day => {
+      return {
+        name: `Killarney Rose`,
+        amount: randomAmount(),
+        date: day,
+        accountId: sheriCredit.account_id,
+        category: 'Shops',
+        category2: 'Supermarkets and Groceries',
+        userId: sheri.id,
+      };
+    }),
+    ...dateArr.map(day => {
+      return {
+        name: `Amazon`,
+        amount: randomAmount(),
+        date: day,
+        accountId: sheriCredit.account_id,
+        category: 'Food and Drink',
+        category2: 'Bar',
+        userId: sheri.id,
+      };
+    }),
+    ...taxiDateArr.map(day => {
+      return {
+        name: randomStore(taxi),
+        amount: randomAmount(),
+        date: day,
+        accountId: sheriCredit.account_id,
+        category: 'Travel',
+        category2: 'Car Service',
+        userId: sheri.id,
+      };
+    }),
+    ...taxiDateArr.map(day => {
+      return {
+        name: randomStore(shops),
+        amount: randomBigAmount(),
+        date: day,
+        accountId: sheriCredit.account_id,
+        category: 'Shops',
+        category2: 'Clothing',
+        userId: sheri.id,
+      };
+    }),
+    {
+      name: 'Madison Bicycle Shop',
+      amount: 50,
+      date: '2018-06-12',
+      accountId: sheriCredit.account_id,
+      category: 'Shops',
+      category2: 'Bicycles',
+      userId: sheri.id,
+    },
+    {
+      name: 'United Airlines',
+      amount: 350,
+      date: '2018-06-19',
+      accountId: sheriCredit.account_id,
+      category: 'Travel',
+      category2: 'Airlines and Aviation Services',
+      userId: sheri.id,
+    },
+    ...onceAMonth.map(day => {
+      return {
+        name: 'Netflix',
+        amount: 12,
+        date: day,
+        accountId: sheriCredit.account_id,
+        category: 'Service',
+        category2: 'Entertainment',
+        userId: sheri.id,
+      };
+    }),
+    ...onceAMonth.map(day => {
+      return {
+        name: 'Touchstone CLimbing',
+        amount: 79,
+        date: day,
+        accountId: sheriCredit.account_id,
+        category: 'Recreation',
+        category2: 'Gyms and Fitness Centers',
+        userId: sheri.id,
+      };
+    }),
+  ];
+
+  const transactions = await Promise.all(
+    allTransactions.map(transaction => Transaction.create(transaction))
+  );
 
   // Wowzers! We can even `await` on the right-hand side of the assignment operator
   // and store the result that the promise resolves to in a variable! This is nice!
